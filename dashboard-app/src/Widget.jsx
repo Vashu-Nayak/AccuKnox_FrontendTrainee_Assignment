5454fe74e58a8213407a40d23d3a962f0535993a
Vashu-Nayak <nvashu1@gmail.com>
2024-08-16 19:47:00 +0530

    updating code

--- dashboard-app/src/Widget.jsx
+++ dashboard-app/src/Widget.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { removeWidget } from './redux/dashboardSlicer';
+import { CiSquareRemove } from "react-icons/ci";
 
 const Widget = ({ widget, categoryId }) => {
   const dispatch = useDispatch();
@@ -13,7 +14,7 @@ const Widget = ({ widget, categoryId }) => {
     <div className="bg-white p-4 rounded-lg shadow-md relative">
       <h3 className="text-lg font-medium text-gray-800">{widget.name}</h3>
       <p className="text-gray-600 mt-2">{widget.text}</p>
-      <button className="absolute top-2 right-2 text-red-500" onClick={handleRemove}>Remove</button>
+      <button className="absolute top-2 right-2 text-red-500 " onClick={handleRemove}><CiSquareRemove /></button>
     </div>
   );
 };
